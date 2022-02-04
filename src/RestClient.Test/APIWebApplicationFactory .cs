c25f9d43628b5ec2f6e8a5c865c7c0ec46b87f50
roessba <bastian.roessl@iosb-ina.fraunhofer.de>
2022-02-04 20:14:19 +0100

    First Running E2E Tests

---
+++ src/RestClient.Test/APIWebApplicationFactory .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace RestClient.Test
+{
+    public class APIWebApplicationFactory : WebApplicationFactory<ServerNet6.Startup>
+    {
+        
+    }
+
+
+
+}
